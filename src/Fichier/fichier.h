98ffc3ad5a6ee0af4d78b9510bbc8615a56d46c6
Younes Ghoniem <60320772+Dhoulnoun@users.noreply.github.com>
2022-02-16 23:41:22 +0100

    purging the whole project of c files keeping fichiers utiles

--- src/Fichier/fichier.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef TSP_FICHIER_H
-#define TSP_FICHIER_H
-
-typedef int ** Graphe;
-void affiche_km(int ** g, int n);
-int lire_data(char * nom, Graphe * g, int *n, int *m);
-#endif //TSP_FICHIER_H
--- src/Main/main.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by omar on 14/02/2022.
-//
-
-#ifndef TSP_MAIN_H
-#define TSP_MAIN_H
-
-#endif //TSP_MAIN_H
