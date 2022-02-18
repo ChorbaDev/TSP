f7d902d749ce40804aed48560c80e90cda5bab11
OmarEll <omarelloumi530@gmail.com>
2022-02-18 10:48:39 +0100

    NN time

--- programmes_utiles/python/NearestNeighbour.py
+++ programmes_utiles/python/NearestNeighbour.py
@@ -1,14 +1,20 @@
 from help import pathCost
 from sys import maxsize
+from time import process_time
 
 def NearestNeighbour(tsp,n,start):
     path=[]
     startTown=start
+    t1_start = process_time()
     while len(path)<n-1:
         minTown=minimum(tsp,path,n,start,startTown)
         path.append(minTown)
         start=minTown
     totalCost=pathCost(tsp,path,start)
+    t1_stop = process_time()
+    print("Elapsed time:", t1_stop, t1_start)
+    print("Elapsed time during the whole program in seconds:",t1_stop-t1_start)
+
     print(path)
     print(totalCost)
 
