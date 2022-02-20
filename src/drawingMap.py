3d6181a9fd086312a785b6157a516ef49002f6b1
Younes Ghoniem <60320772+Dhoulnoun@users.noreply.github.com>
2022-02-20 15:24:34 +0100

    drawing the map

--- src/drawingMap.py
+++ src/drawingMap.py
@@ -4,7 +4,18 @@ import openpyxl
 import scipy.cluster.hierarchy as sch
 import pandas as pd
 
-col_list=["X","Y"]
-df = pd.read_excel('../communes/communes.xlsx', usecols=col_list)
-plt.scatter(df["X"],df["Y"],s=5, c='k', marker='o')
-plt.show()
+
+def scatterCities(file='../communes/communes.xlsx', scale=10):
+    col_list=["X","Y"]
+    df = pd.read_excel(file, usecols=col_list, nrows=scale)
+    plt.scatter(df["X"],df["Y"],s=5, c='k', marker='o')
+    plt.show()
+
+def drawPath(file='../communes/communes.xlsx', scale=10, solution=[]):
+    """
+    draws the path between two cities according to a given list
+    :param solution:
+    :param file:
+    :param scale:
+    :return:
+    """
