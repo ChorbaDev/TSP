502dc437b1b4746a688879a27c306f65a554245f
Younes <younes.ghoniem@gmail.com>
2022-02-21 12:20:20 +0100

    from mathplotlib to plotly

--- src/utils/drawingMap.py
+++ src/utils/drawingMap.py
@@ -2,16 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import openpyxl
 import scipy.cluster.hierarchy as sch
+import plotly.express as px
 import pandas as pd
 
 
-def scatterCities(file='../communes/communes.xlsx', scale=10):
-    col_list=["X","Y"]
+def scatterCities(file="../../communes/communes.xlsx", scale=777):
+    col_list = ["X", "Y", "nom_commune_majuscule"]
     df = pd.read_excel(file, usecols=col_list, nrows=scale)
-    plt.scatter(df["X"],df["Y"],s=5, c='k', marker='o')
-    plt.show()
+    fig = px.scatter(df, x=col_list[0], y=col_list[1], hover_name=col_list[2])
+    fig.show()
 
-def drawPath(file='../communes/communes.xlsx', scale=10, solution=[]):
+
+def drawPath(file='../../communes/communes.xlsx', scale=10, solution=[]):
     """
     draws the path between two cities according to a given list
     :param solution:
