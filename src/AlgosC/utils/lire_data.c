ad163a9ec0cb9b3fdb24ac61f7c60fdf8c8b1b5b
OmarEll <omarelloumi530@gmail.com>
2022-02-23 12:09:34 +0100

    BruteForce in C

--- src/AlgosC/utils/lire_data.c
+++ src/AlgosC/utils/lire_data.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 //#include "../../src/Algorithmique/PPV/PPV.c"
-#include "/home/omar/TSP/src/AlgosC/C/BruteForce/BruteForce.c"
+#include "../C/BruteForce/BruteForce.c"
 //#include "NearestNeighbour.c"
 //#include "Random.c"
 //#include "BruteForce.c"
